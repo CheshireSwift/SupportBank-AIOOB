b62d0a75c8fa8420b2e9e024c073dd99d2bcbeda
Sam Collard <swift@swifteagle.co.uk>
2018-07-20 13:44:07 +0100

    Revert "Delete all"
    This reverts commit e9380edc09910b0d249d670128d2e64b0f082742.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const readlineSync = require('readline-sync');
+const log4js = require('log4js');
+
+const processCommand = require('./commands.js');
+
+log4js.configure({
+    appenders: {
+        file: {
+            type: 'fileSync',
+            filename: 'logs/debug.log'
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['file'],
+            level: 'debug'
+        }
+    }
+});
+const logger = log4js.getLogger('index.js');
+logger.info('Logging Initialised');
+
+if (process.argv.length > 2) {
+    console.log('Command line arguments are not supported.');
+}
+
+const accounts = new Map();
+readlineSync.promptLoop((input) => processCommand(input, accounts));
